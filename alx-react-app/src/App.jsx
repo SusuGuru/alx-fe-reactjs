3ba878fc8abffd96587f466c2f4c86380442d884
SusuGuru <sorayabrayka@gmail.com>
2025-07-14 16:36:54 +0000

    Fix: App.jsx for Task 2 - only Header, MainContent, Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
-import UserProfile from './components/UserProfile';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
