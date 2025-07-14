b94089e19afeba20d0a8d32d8672115b88ea707f
SusuGuru <sorayabrayka@gmail.com>
2025-07-14 16:20:48 +0000

    Fix: remove extra components for ALX checker (Header, MainContent, Footer only)

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -10,12 +8,6 @@ function App() {
     <div>
       <Header />
       <MainContent />
-      <WelcomeMessage />
-      <UserProfile
-        name="Soraya"
-        age="23"
-        bio="Aspiring full-stack developer, passionate about React!"
-      />
       <Footer />
     </div>
   );
