17a0480d96f8dbeac0f53bd1110c48ac2b612380
SusuGuru <sorayabrayka@gmail.com>
2025-07-21 19:57:11 +0000

    Apply CSS inline

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import UserDetails from './UserDetails';
+import UserContext from './UserContext';
 
 function ProfilePage() {
+  const user = useContext(UserContext);
+
   return (
     <div>
-      <h1>Welcome to the Profile Page</h1>
+      <h1>Welcome to the Profile Page, {user.name}!</h1>
       <UserDetails />
     </div>
   );
