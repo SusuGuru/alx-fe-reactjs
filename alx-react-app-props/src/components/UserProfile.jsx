14a0cae0ef7dbfaa4fc49b0aed2b1689a09bba5f
SusuGuru <sorayabrayka@gmail.com>
2025-07-22 16:11:30 +0000

    Use Context API

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
+import { useContext } from 'react';
+import UserContext from './UserContext';
+import UserDetails from './UserDetails';
+
+function UserProfile() {
+  const user = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h1>Welcome to the Profile Page, {user.name}!</h1>
+      <UserDetails />
     </div>
   );
-};
+}
 
 export default UserProfile;
