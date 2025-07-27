7f0ff5130a0205d8723b0e99eb1a911672e62994
SusuGuru <sorayabrayka@gmail.com>
2025-07-27 20:17:35 +0000

    Add Zustand store, AddRecipeForm, and RecipeList components

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,10 +2,15 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
+  // Add a new recipe
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  // Replace all recipes (used to reset or load recipes)
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
 
 export default useRecipeStore;
