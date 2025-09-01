a3763671513091389bf03ce9508af65b6f76c69a
SusuGuru <sorayabrayka@gmail.com>
2025-09-01 16:50:12 +0000

    Add React Query demo with PostsComponent and App styling

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PostsComponent from './components/PostsComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
